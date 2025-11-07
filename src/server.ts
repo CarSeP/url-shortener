f2732a102e08b7408d2ba861d46bd2d56e741026
CarSeP <vjpd64@gmail.com>
2025-11-07 10:00:55 -0400

    Add handling for mismatched routes on the server

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,9 @@ import { routes } from "./routes";
 export const createServer = (port: string) => {
   return Bun.serve({
     port,
-    routes
+    routes,
+    fetch() {
+      return Response.json({ error: "Unmatched route" });
+    },
   });
-}
+};
