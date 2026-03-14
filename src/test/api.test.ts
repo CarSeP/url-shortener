0a466d6f57ec92f7f304f1052c7e230c1c66f062
CarSeP <vjpd64@gmail.com>
2026-03-14 12:09:01 -0400

    test: make test functions asynchronous for URL shortener

--- src/test/api.test.ts
+++ src/test/api.test.ts
@@ -5,23 +5,23 @@ const URL = "https://example.com/abc/123";
 const INVALID_VALUE = "qwe123";
 let code = "";
 
-test("should create short URL when valid URL is provided", () => {
-  const data = createShortUrl(URL, "");
+test("should create short URL when valid URL is provided", async () => {
+  const data = await createShortUrl(URL, "");
   if (data) code = data.code;
   expect(data).toBeTruthy();
 });
 
-test("should redirect to original URL when valid short code is provided", () => {
-  const data = getShortUrl(code);
+test("should redirect to original URL when valid short code is provided", async () => {
+  const data = await getShortUrl(code);
   expect(data).toBeTruthy();
 });
 
-test("should return false when invalid URL is provided", () => {
-  const data = createShortUrl(INVALID_VALUE, "");
+test("should return false when invalid URL is provided", async () => {
+  const data = await createShortUrl(INVALID_VALUE, "");
   expect(data).toBeFalsy();
 });
 
-test("should return false when invalid short code is provided", () => {
-  const data = getShortUrl(INVALID_VALUE);
+test("should return false when invalid short code is provided", async () => {
+  const data = await getShortUrl(INVALID_VALUE);
   expect(data).toBeFalsy();
 });
